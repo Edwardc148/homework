3da91048bcc8bb28f3035773fb2579900b98f5ab
Edward <edwardcheng148@gmail.com>
2018-01-23 20:14:20 -0800

    add W7D3 homework

---
+++ W7D3/components/root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import GiphysSearchContainer from './giphys_search_container';
+
+const Root = () => {
+  return (
+    <h1>Hello</h1>
+  );
+};
+
+export default Root;
